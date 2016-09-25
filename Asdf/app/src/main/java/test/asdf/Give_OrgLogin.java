020aeda6b6dd4bc139701b48a0af52ea17627666
Rohit Kulkarni <Rohit Kulkarni>
2016-09-25 06:47:33 -0700

    give buttons

---
+++ Asdf/app/src/main/java/test/asdf/Give_OrgLogin.java
@@ -0,0 +1,88 @@
+package test.asdf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by jzou on 9/24/16.
+ */
+public class Give_OrgLogin extends AppCompatActivity implements View.OnClickListener{
+
+
+    private Button signup;
+
+    private EditText name;
+
+    private GoogleApiClient client;
+    private DatabaseReference firebaseRef;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+    private static final String TAG = "New Post Activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.req_info);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        signup = (Button)findViewById(R.id.signupButton);
+        name = (EditText)findViewById(R.id.nameInputField);
+        signup.setOnClickListener(this);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        }); */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view)
+    {
+        if ( view == signup){
+            String name_val = name.getText().toString();
+            myRef.setValue(name_val);
+            
+            startActivity(new Intent(this,Give_OrgSelect.class));
+        }
+    }
+
+}
---
+++ Asdf/app/src/main/java/test/asdf/Give_OrgSelect.java
@@ -0,0 +1,83 @@
+package test.asdf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by jzou on 9/24/16.
+ */
+public class Give_OrgSelect extends AppCompatActivity implements View.OnClickListener{
+
+
+    private Button confirmbutton;
+
+    private GoogleApiClient client;
+    private DatabaseReference firebaseRef;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+    private static final String TAG = "New Post Activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.req_info);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        confirmbutton = (Button)findViewById(R.id.confirmbutton);
+        confirmbutton.setOnClickListener(this);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        }); */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view)
+    {
+        if ( view == confirmbutton){
+            startActivity(new Intent(this,Request_Choices.class));
+        }
+    }
+
+}
